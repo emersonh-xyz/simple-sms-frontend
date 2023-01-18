1e609506686cf5ec3f0b949d83213fe6d0d07bbe
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-01-17 23:39:42 -0500

    fixed center alignment for homie dog noah

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mt-auto p-4 bg-base-300 text-base-content ">
+    <footer className="mt-auto text-center p-4 bg-base-300 text-base-content ">
       <div>
         <p>Copyright © 2022 - All right reserved by Simple SMS</p>
       </div>
