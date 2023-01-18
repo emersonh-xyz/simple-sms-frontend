55a89a1b51bb5d05bb5171f6e673a7e1daae1106
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-01-18 12:13:24 -0500

    Created  a simple 404 page

---
+++ pages/404.js
@@ -0,0 +1,36 @@
+
+import { Icon } from "@iconify-icon/react"
+import Head from "next/head"
+import Image from "next/image"
+import Footer from "../components/Footer/index"
+import Navbar from "../components/Navbar"
+
+export default function Custom404() {
+    return (
+        <div className="flex flex-col h-screen ">
+            <Head>
+                <title>Simple SMS</title>
+            </Head>
+
+            <Navbar />
+
+            <>
+
+                <div className="grid h-screen place-items-center">
+                    <div>
+                        <Icon className="mx-auto" height={250} width={500} alt="A very sad looking cat" icon={"ci:sad"} />
+                        <h1 className="text-3xl font-bold text-center mb-2">404 error</h1>
+                        <h1 className="text-xl text-center mb-2">The page you were looking for could not be found.</h1>
+
+                        <button onClick={() => window.location.href = "/"} className="btn btn-primary w-full mt-2">Return home</button>
+                    </div>
+                </div>
+
+
+            </>
+
+            <Footer></Footer>
+
+        </div >
+    )
+}
