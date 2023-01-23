5acc328cecd3be34f59fb1cef779d77fc84832e5
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-01-22 23:58:35 -0500

    PUSH

--- components/WaveBanner/index.js
+++ components/WaveBanner/index.js
@@ -8,29 +8,34 @@ export default function WaveBanner({ serviceSelectorRef }) {
 
 
     return (
-        <section style={{ backgroundColor: `#FF3CAC`, background: `linear-gradient(225deg, #FF3CAC 0%, #784BA0 50%, #2B86C5 100%)` }}>
+        <section style={{ backgroundColor: `#FF3CAC`, background: `linear-gradient(225deg, #2B86C5 0%, #784BA0 50%, #FF3CAC 100%)` }}>
 
 
 
-            <div className="absolute left-60">
+            <div className="absolute left-20">
                 <div className="text-white ">
-                    <p className="text-4xl font-medium">Simple SMS</p>
-                    <p className="text-md mt-1 font-thin">Easily select from 100+ services, monitor texts, secure checkout & money-back guarantee.</p>
-                    <p className="text-md font-thin">User-friendly, affordable premium SMS number provider.</p>
-
-                </div>
-
-                <div className="mt-4">
-                    <button onClick={() => executeScroll()} className="text-white border-2 font-medium border-white hover:scale-95 hover:bg-white hover:text-black px-6 py-2 rounded-full outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" type="button"
-                    >
-                        Purchase Number
-                    </button>
-                    <button onClick={() => executeScroll()} className="text-[#eab308] border-2 border-[#eab308] font-medium  hover:bg-[#eab308] hover:text-black hover:scale-95 px-6 py-2 rounded-full outline-none focus:outline-none mr-1 ease-linear transition-all duration-150 ml-2" type="button"
-                    >
-                        About
-                    </button>
+                    <p className="text-xl font-medium ">Simple SMS</p>
                 </div>
             </div>
+
+            <p className="text-4xl font-medium text-center ">The premium <a className="text-primary">all-in-one</a> SMS number provider</p>
+            <p className="text-md font-thin mt-2">Effortlessly purchase affordable phone numbers with our user-friendly, elegant interface. </p>
+
+            <button onClick={() => executeScroll()} className=" mt-2 text-white border-2 font-medium border-white hover:scale-95 hover:bg-white hover:text-black px-4 py-2 text-sm rounded-full outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" type="button"
+            >
+                Purchase Number
+            </button>
+
+
+
+            <div className="absolute right-20">
+
+                <button onClick={() => executeScroll()} className="text-[#eab308] border-2 border-[#eab308] font-medium  hover:bg-[#eab308] hover:text-black hover:scale-95 px-4 py-2 text-sm  rounded-full outline-none focus:outline-none mr-1 ease-linear transition-all duration-150 ml-2" type="button"
+                >
+                    About
+                </button>
+            </div>
+
 
 
 
