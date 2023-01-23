5773c7e1c4f7f596a1f5904b34eb4680e63246df
NSGOmega <emersontrickz@gmail.com>
2023-01-23 16:18:50 -0500

    Adjusted alignment on mobile

--- components/WaveBanner/index.js
+++ components/WaveBanner/index.js
@@ -11,40 +11,45 @@ export default function WaveBanner({ serviceSelectorRef, featureRef }) {
     return (
         <section style={{ backgroundColor: `#FF3CAC`, background: `linear-gradient(225deg, #2B86C5 0%, #784BA0 50%, #FF3CAC 100%)` }}>
 
-            <div className="md:mx-left lg:absolute lg:left-20 flex justify-start">
-                <div className="text-white ">
-                    <p className="text-xl font-medium transform transition-all duration-150 ease-out scale-100">Simple SMS</p>
+
+
+
+            <div>
+                <div className="md:absolute left-20">
+                    <div className="text-white ">
+                        <p className="text-xl font-medium transform transition-all duration-150 ease-out scale-100">Simple SMS</p>
+                    </div>
                 </div>
-            </div>
-
-            <div className="text-center">
-                <p className="lg:text-3xl text-sm font-medium text-center ">The premium <a href="#" className="text-primary">all-in-one</a> SMS number provider</p>
-                <p className="lg:text-md text-xs font-thin mt-2 ">Effortlessly purchase affordable phone numbers with our user-friendly, elegant interface. </p>
-            </div>
-            <button onClick={() => executeScrollOnServiceSelector()} className="inline-flex items-center mt-2 text-white border-2 font-medium border-white hover:scale-95 hover:bg-white hover:text-black px-4 py-2 text-sm rounded-full outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" type="button"
-            >
-                <Icon className="mr-1" icon="ic:baseline-shopping-cart"></Icon>Purchase Number
-            </button>
 
 
-            <div className="lg:absolute right-20 mt-3">
+                <p className="md:text-2xl text-sm font-medium md:text-center">The premium <a href="#" className="text-primary">all-in-one</a> SMS number provider</p>
+                <p className="md:text-md text-xs font-thin mt-2 md:text-center">Effortlessly purchase affordable phone numbers with our user-friendly, elegant interface. </p>
 
-                <button onClick={() => executeScrollOnFeature()} className="text-[#eab308] border-2 border-[#eab308] font-medium  hover:bg-[#eab308] hover:text-black hover:scale-95 px-4 py-2 text-sm  rounded-full outline-none focus:outline-none mr-1 ease-linear transition-all duration-150 ml-2" type="button"
+                <div className="md:text-center">
+                    <button onClick={() => executeScrollOnServiceSelector()} className="inline-flex items-center mt-2 text-white border-2 font-medium border-white hover:scale-95 hover:bg-white hover:text-black px-4 py-2 text-sm rounded-full outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" type="button"
+                    >
+                        <Icon className="mr-1" icon="ic:baseline-shopping-cart"></Icon>Purchase Number
+                    </button>
+                </div>
+
+
+
+
+                <button onClick={() => executeScrollOnFeature()} className="inline-flex items-center md:absolute right-20 top-11 mt-2 lg:mt-0 text-[#eab308] border-2 border-[#eab308] font-medium  hover:bg-[#eab308] hover:text-black hover:scale-95 px-4 py-2 text-sm rounded-full outline-none focus:outline-none ease-linear transition-all duration-150" type="button"
                 >
-                    About
+                    <Icon className="mr-1" icon="ic:baseline-info"></Icon>About
                 </button>
+
+
+
+
+                <div className="invisible md:visible custom-shape-divider-bottom-1674077680 drop-shadow-lg">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+                    </svg>
+                </div>
+
             </div>
-
-
-
-
-            <div className="invisible lg:visible custom-shape-divider-bottom-1674077680 drop-shadow-lg">
-                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
-                </svg>
-            </div>
-
-
         </section>
     )
 }
