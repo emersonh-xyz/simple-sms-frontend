08a7e3633ad20cf0cf99358f8fa8d077dd9bc7ee
NSGOmega <emersontrickz@gmail.com>
2023-01-26 10:09:19 -0500

    Update index.js

--- pages/order/[uuid]/index.js
+++ pages/order/[uuid]/index.js
@@ -107,7 +107,7 @@ const Order = ({ props }) => {
       }
 
 
-      {!isLoading &&
+      {/* {!isLoading && TODO:
         <div className="alert alert-warning shadow-lg rounded-none ">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
@@ -120,7 +120,7 @@ const Order = ({ props }) => {
             <button className="btn btn-sm">See</button>
           </div>
         </div>
-      }
+      } */}
 
       {!isLoading ?
 
