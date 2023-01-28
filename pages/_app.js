63c0f2001754d01a1959e60287f41d186785122a
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-01-28 13:50:45 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,13 @@ import { Analytics } from '@vercel/analytics/react';
 import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Analytics />
+      <Component {...pageProps} />;
+    </>
+  )
+
 }
 
 export default App;
