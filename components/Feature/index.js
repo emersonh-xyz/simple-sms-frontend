dd01a44b168aca23308809e917ae4377b9e6897a
NSGOmega <emersontrickz@gmail.com>
2023-02-20 15:09:55 -0500

    Adjusted Feature for mobile / desktop.

--- components/Feature/index.js
+++ components/Feature/index.js
@@ -3,15 +3,15 @@ export default function Feature({ featureRef }) {
   return (
     <section ref={featureRef} className="bg-base-300">
 
-      <div class="p-8">
-        <div class="flex felx-col items-center justify-center">
+      <div class="p-8 lg:mr-14">
+        <div class="flex felx-col items-center justify-center ">
           <span
-            class="rounded-full bg-primary px-2 py-1 text-gray-900  text-sm"
+            class="rounded-full bg-primary px-2 py-1 text-gray-900 text-sm"
           >
             About
           </span>
         </div>
-        <h1 class="text-md w-full lg:text-4xl font-medium text-center mt-6">
+        <h1 class="text-md w-full lg:text-3xl font-medium text-center mt-6">
           Simple SMS is a secure and easy to use service.
         </h1>
         <p class="text-center mt-6 text-lg font-light ">
@@ -19,46 +19,44 @@ export default function Feature({ featureRef }) {
         </p>
       </div>
 
-      <div class="">
-        <div class="p-8 text-center">
+      <div class="flex lg:flex-row flex-col justify-center items-center text-center">
+        <div class="p-8 ">
 
           <Icon className="" width="50px" icon="material-symbols:no-accounts"></Icon>
 
-          <h2 class="uppercase mt-6 text-red-600 font-medium mb-3 ">
-            No Sign up Required
+          <h2 class="uppercase text-primary font-medium mb-2">
+            No SignUp
           </h2>
-          <p class="font-light text mb-3 text-sm ">
+          <p class="">
             Enjoy privacy benefits and avoid sharing personal information.
           </p>
 
         </div>
 
-        <div class="p-8 text-center">
+        <div class="p-8">
 
-          <Icon width="50px" icon="mdi:encryption-secure"></Icon>
 
-          <h2 class="uppercase mt-6 text-green-600 font-medium mb-3">
+          <Icon class="" width="50px" icon="bxl:venmo"></Icon>
+
+          <h2 class="uppercase text-primary font-medium mb-2">
             Venmo Secured
           </h2>
-          <p class="font-light mb-3 text-sm">
+          <p class="">
             Checkout is secure and seamless through Venmo, ensuring the safety of your transaction.
           </p>
 
+
         </div>
-        <div class="p-8 text-center">
+        <div class="p-8">
 
-          <Icon width="50px" icon="ic:baseline-support-agent"></Icon>
+          <Icon width="50px" class="" icon="ic:baseline-discord"></Icon>
 
-          <h2 class="uppercase mt-6 text-indigo-500 font-medium mb-3 text-center">
-            Support Discord
+          <h2 class="uppercase text-primary font-medium mb-2">
+            <a class="underline" href="https://discord.gg/YWrdUzDups">Support Discord</a>
           </h2>
-          <p class="font-light mb-3 text-sm">
+          <p class="">
             Do not hesitate to reach out to us on our Discord channel for any assistance you may need.
           </p>
-          <a href="https://discord.gg/YWrdUzDups" className="text-indigo-600 hover:text-indigo-700" >
-            Join our Discord
-
-          </a>
         </div>
       </div>
     </section >
