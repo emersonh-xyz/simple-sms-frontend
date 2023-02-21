6ba6c95d59453ab3ad3d98b0bb1d5ee97ea2b583
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-02-21 18:05:15 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -93,7 +93,7 @@ export default function Home() {
     });
 
     socket.io.on("reconnect", () => {
-      socket.emit("get-order", orderId)
+      socketRef.current.emit("get-order", orderId)
     });
 
 
