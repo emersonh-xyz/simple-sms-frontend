0e633cb011b1e92bc2f8e60d38540c36e921379f
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-02-21 18:44:54 -0500

    Added a button to reopen venmo incase it closes

--- components/CheckoutDetails/index.js
+++ components/CheckoutDetails/index.js
@@ -125,7 +125,7 @@ export default function CheckoutDetails({ service, startVenmoOrder, venmoRef, is
                     venmoRef.current.closePaymentWindow();
                     location.reload();
                   }}
-                  className="flex justify-evenly btn btn-wide border-none bg-[#008CFF] hover:bg-error text-white"
+                  className="flex justify-evenly btn btn-wide border-none hover:bg-error text-white"
                   onMouseOver={() => { setCancelButtonHovered(true); }}
                   onMouseOut={() => { setCancelButtonHovered(false); }}
                 >
@@ -168,10 +168,31 @@ export default function CheckoutDetails({ service, startVenmoOrder, venmoRef, is
                 </button>
               </div>
             )}
-
-
-
           </div>
+
+          {isVenmoCheckout && <button
+            class="btn bg-[#008CFF] text-white"
+            onClick={() => {
+              if (startVenmoOrder(service.service_price, service.service_name)) {
+                setVenmoCheckout(true);
+              } else {
+                setShouldShowConnectionError(true);
+              }
+            }}>
+
+            <svg
+              id="Layer_1"
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              className="w-5 h-5 mr-1"
+            >
+              <path
+                d="M444.17,32H70.28C49.85,32,32,46.7,32,66.89V441.6C32,461.91,49.85,480,70.28,480H444.06C464.6,480,480,461.8,480,441.61V66.89C480.12,46.7,464.6,32,444.17,32ZM278,387H174.32L132.75,138.44l90.75-8.62,22,176.87c20.53-33.45,45.88-86,45.88-121.87,0-19.62-3.36-33-8.61-44L365.4,124.1c9.56,15.78,13.86,32,13.86,52.57C379.25,242.17,323.34,327.26,278,387Z"
+                fill="white"
+              ></path>
+            </svg>Open Venmo</button>}
+
           <div className="text-sm w-64">
             By clicking Checkout with Venmo, you agree to our <label htmlFor="tos" className="underline text-primary">Terms and Conditions</label>
           </div>
